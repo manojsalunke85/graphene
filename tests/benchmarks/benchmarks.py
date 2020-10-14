b107fee99f001354d9eae33dfeac2842a2d327a9
Wojtek Porczyk <woju@invisiblethingslab.com>
2020-10-14 19:27:47 +0200

    [tests] Buildable benchmarks
    This allows for benchmarking multiple commits at once and bisecting them
    too.

--- tests/benchmarks/benchmarks.py
+++ tests/benchmarks/benchmarks.py
@@ -1,24 +1,29 @@
-import os
-import pathlib
-import subprocess
+from . import Exec
 
+# pylint: disable=invalid-name
 
-def setup():
-    os.environ['SGX'] = '1'
-
-_cwd = pathlib.Path(__file__).parent.absolute()
-def _run(stem):
-    subprocess.run([os.fspath('../../Runtime/pal_loader'), stem + '.manifest.sgx'],
-        check=True, cwd=os.fspath(_cwd))
-
+helloworld = Exec('helloworld', manifest_template='manifest.template')
+@helloworld.add_setup
 def time_000_helloworld():
-    _run('helloworld')
+    helloworld.run_in_graphene()
+time_000_helloworld.setup = helloworld.setup
 
+write_1e4 = Exec('write_1e4', manifest_template='manifest.template')
+@write_1e4.add_setup
 def time_014_write_1e4():
-    _run('write_1e4')
+    write_1e4.run_in_graphene()
+
+write_1e5 = Exec('write_1e5', manifest_template='manifest.template')
+@write_1e5.add_setup
 def time_015_write_1e5():
-    _run('write_1e5')
+    write_1e5.run_in_graphene()
+
+write_1e6 = Exec('write_1e6', manifest_template='manifest.template')
+@write_1e6.add_setup
 def time_016_write_1e6():
-    _run('write_1e6')
+    write_1e6.run_in_graphene()
+
+write_1e7 = Exec('write_1e7', manifest_template='manifest.template')
+@write_1e7.add_setup
 def time_017_write_1e7():
-    _run('write_1e7')
+    write_1e7.run_in_graphene()
